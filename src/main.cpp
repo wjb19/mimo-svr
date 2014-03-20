bd504948b63997cfe26116b5f16717973ef38e8e
wjb19 <whiskeyjulietb@gmail.com>
2014-03-20 16:32:10 -0400

    added data

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,7 @@
 
 
 #include <iostream>
+#include <stdlib.h>
 #include "msvr.hpp"
 #include "armadillo"
 #include "utilities.hpp"
@@ -46,28 +47,42 @@ using namespace mimo_svr;
 int main(int argc, char * argv[]){
 
 
+	if (argc!=9){
+
+		cout << "usage : <xtrain.csv> <xtest.csv> <ytrain.csv> <ytest.csv> <type=\"lin\" OR \"poly\" OR \"rbf\"> <epsilon> <C> <param>" << endl;
+		exit(0);
+	}
+
 	mat Xtest, Xtrain, Ytest, Ytrain;
 	Xtrain.load(argv[1]);
 	Xtest.load(argv[2]);
 	Ytrain.load(argv[3]);
 	Ytest.load(argv[4]);
+	char * type = argv[5];
+	double epsi = strtod(argv[6],NULL);
+	double C = strtod(argv[7],NULL);
+	double mult = strtod(argv[8],NULL);
 
-	double param 	= mean_all(pdist(Xtest));
-	double epsi 	= 2.0f,C = 5.0f, tol = 1e-20;
+	double param 	= mult*mean_all(pdist(Xtest));
+	//double epsi 	= 2.0f,C = 5.0f, tol = 1e-20;
+	double tol = 1e-20;
 
-	msvr trainA(Xtrain,Ytrain,"rbf",C,epsi,param,tol);
-	mat Ktest 	= k_mat(trans(Xtest),trans(Xtrain),"rbf",param);
+	msvr trainA(Xtrain,Ytrain,type,C,epsi,param,tol);
+	mat Ktest 	= k_mat(trans(Xtest),trans(Xtrain),type,param);
 	mat Ypred 	= Ktest*trainA.getBeta();
 
 	double length = size(Ypred,0);
-	mat diff = sqrt(sum(pow((Ypred-Ytest),2))/length);
+	//mat diff = sqrt(sum(pow((Ypred-Ytest),2))/length);
+	
+	mat diff = sum(abs(1-Ypred/Ytest))/length;
 
-	cout << "RMSE: " << endl;
+
+	//cout << "RMSE: " << endl;
 	cout << diff << endl;
-	cout << "Ypred: " << endl;
-	cout << Ypred << endl;
-	cout << "Ytest: " << endl;
-	cout << Ytest << endl;
+	//cout << "Ypred: " << endl;
+	//cout << Ypred << endl;
+	//cout << "Ytest: " << endl;
+	//cout << Ytest << endl;
 	
 	return 0;
 
